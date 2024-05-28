e89c1e4d16357c88afdca211961f3440a3318104
vtr-ml <167699928+vtr-ml@users.noreply.github.com>
2024-05-28 15:11:18 +0530

    Update VTR_ML.py

--- VTR_ML.py
+++ VTR_ML.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import streamlit_authenticator as stauth
-import streamlit as st 
-import mysql.connector
-import mysql
-from firebase_admin import credentials
-from firebase_admin import auth
-from streamlit_option_menu import option_menu
-from PIL import Image
+
 
 
 st.title("VTR Machine Learning")
