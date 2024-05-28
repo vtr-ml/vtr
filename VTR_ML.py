418da369fe9b03b76257de8b7a5af5d667273c56
vtr-ml <167699928+vtr-ml@users.noreply.github.com>
2024-05-28 16:14:30 +0530

    Update VTR_ML.py

--- VTR_ML.py
+++ VTR_ML.py
@@ -1,4 +1,26 @@
 import streamlit as st
 
-st.title("VTR Machine Learning")
+st.header ("VTR Machine Learning", divider='rainbow')
 
+col1, col2= st.columns(2)
+
+with st.sidebar:
+
+    st.header(":yellow[List of contents]")
+    main = st.button("Main Menu")
+
+    bt = st.button("My Resume")
+if bt:
+    with col1:
+        st.write ("L LOGIDHASAN", 
+                  "vetri.dreams2010@gmail.com ")
+                  
+                  
+                  
+
+
+if main:
+    st.header("Machine Learning")
+    st.write ("Machine Learning is a subset of artificial intelligence(AI) that focus on learning from data to develop an algorithm that can be used to make a prediction")
+    st.write("A machine learning algorithm works by learning patterns and relationships from data to make predictions or decisions without being explicitly programmed for each task.")
+    st.divider()
