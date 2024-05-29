451ea4a9b92e927203934da589e2ccc79725f10a
vtr-ml <167699928+vtr-ml@users.noreply.github.com>
2024-05-29 10:04:40 +0530

    Update VTR_ML.py

--- VTR_ML.py
+++ VTR_ML.py
@@ -7,14 +7,15 @@ from IPython.display import display
 from IPython.display import Markdown
 genai.configure(api_key="AIzaSyD5xP91E7lR4shBS1CqGGzQIUWNuBLsqx8")
 
-st.set_page_config(page_title="Home", page_icon=":earth_asia:", layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="VTR", page_icon=":earth_asia:", layout="wide", initial_sidebar_state="auto", menu_items=None)
+
 
 col1, col2= st.columns(2)
 
 with st.sidebar:
 
     st.header(":yellow[List of contents]")
-    page_names = ["Home", "My Resume", "VTR ChatGPT"]
+    page_names = ["Home", "My Resume", "VTR ChatGPT", "Need Help?"]
     page = st.radio("Navigation", page_names)
 
 if page == "Home":
@@ -27,16 +28,25 @@ if page == "Home":
 if page =="My Resume":
     st.write("L. Logidhasan")
 
+
 if page =="VTR ChatGPT":
     with col1:
         st.header(":yellow[Welcome to VTR ChatGPT]")
         Enter=st.text_input("Please enter")
         Start = st.button("Generate")
+        messages = st.container(height=1200)
     if Start:
         model = genai.GenerativeModel('gemini-pro')
         response = model.generate_content(Enter)
         X = response.text
-        st.write (X)
+        messages.write (X)
+
+if page =="Need Help?":   
+    st.header("Any Query")
+    msg = st.container(height=600)
+    if prompt := st.chat_input("Say something"):
+        msg.chat_message("user").write(prompt)
+        msg.chat_message("assistant").write(f"Echo: {prompt}")
 
 
             
