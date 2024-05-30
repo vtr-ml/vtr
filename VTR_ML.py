e862ce821d0c9c0792f9727e999c10b598fa2e85
vtr-ml <167699928+vtr-ml@users.noreply.github.com>
2024-05-31 00:27:43 +0530

    Update VTR_ML.py

--- VTR_ML.py
+++ VTR_ML.py
@@ -48,9 +48,10 @@ if page =="Need Help?":
         msg.chat_message("user").write(prompt)
         msg.chat_message("assistant").write(f"Echo: {prompt}")
 
+          
+st.sidebar.page_link("pages/Youtube_Harvesting.py", label="Youtube_Harvesting")
 
-            
-
+st.sidebar.page_link("Youtube_Harvesting.py", label="Youtube")
 
 
 
