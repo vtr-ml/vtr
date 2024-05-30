3d3e85d26e764073cc4627a391bfbe0772c03a43
vtr-ml <167699928+vtr-ml@users.noreply.github.com>
2024-05-30 23:31:29 +0530

    Update VTR_ML.py

--- VTR_ML.py
+++ VTR_ML.py
@@ -15,7 +15,7 @@ col1, col2= st.columns(2)
 with st.sidebar:
 
     st.header(":yellow[List of contents]")
-    page_names = ["Home", "My Resume", "VTR ChatGPT", "Need Help?"]
+    page_names = ["Home", "My Resume", "VTR ChatGPT", "Need Help?", "BMI Calculator"]
     page = st.radio("Navigation", page_names)
 
 if page == "Home":
@@ -49,7 +49,56 @@ if page =="Need Help?":
         msg.chat_message("assistant").write(f"Echo: {prompt}")
 
 
-            
+if page =="BMI Calculator":
+    st.title('Welcome to BMI Calculator')
+    weight = st.number_input("Enter your weight (in kgs)")
+    status = st.radio('Select your height format: ',
+                      ('cms', 'meters', 'feet'))
+    if(status == 'cms'):
+        height = st.number_input('Centimeters')
+        try:
+            bmi = weight / ((height/100)**2)
+        except:
+            st.text("Enter some value of height")
+ 
+    elif(status == 'meters'):
+    # take height input in meters
+        height = st.number_input('Meters')
+ 
+        try:
+            bmi = weight / (height ** 2)
+        except:
+            st.text("Enter some value of height")
+ 
+    else:
+    # take height input in feet
+        height = st.number_input('Feet')
+ 
+    # 1 meter = 3.28
+        try:
+            bmi = weight / (((height/3.28))**2)
+        except:
+            st.text("Enter some value of height")
+ 
+# check if the button is pressed or not
+    if(st.button('Calculate BMI')):
+ 
+    # print the BMI INDEX
+        st.text("Your BMI Index is {}.".format(bmi))
+ 
+    # give the interpretation of BMI index
+        if(bmi < 16):
+            st.error("You are Extremely Underweight")
+        elif(bmi >= 16 and bmi < 18.5):
+            st.warning("You are Underweight")
+        elif(bmi >= 18.5 and bmi < 25):
+            st.success("Healthy")
+        elif(bmi >= 25 and bmi < 30):
+            st.warning("Overweight")
+        elif(bmi >= 30):
+            st.error("Extremely Overweight")
+    
+
 
 
 
