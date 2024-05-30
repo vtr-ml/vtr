19ab4cab567a10a1fd6b44dbd09dd96d24207069
vtr-ml <167699928+vtr-ml@users.noreply.github.com>
2024-05-31 00:22:28 +0530

    Update VTR_ML.py

--- VTR_ML.py
+++ VTR_ML.py
@@ -5,6 +5,10 @@ import textwrap
 import google.generativeai as genai
 from IPython.display import display
 from IPython.display import Markdown
+import pymongo
+import pandas as pd
+import mysql.connector
+
 genai.configure(api_key="AIzaSyD5xP91E7lR4shBS1CqGGzQIUWNuBLsqx8")
 
 st.set_page_config(page_title="VTR", page_icon=":earth_asia:", layout="wide", initial_sidebar_state="auto", menu_items=None)
@@ -15,7 +19,7 @@ col1, col2= st.columns(2)
 with st.sidebar:
 
     st.header(":yellow[List of contents]")
-    page_names = ["Home", "My Resume", "VTR ChatGPT", "Need Help?", "BMI Calculator"]
+    page_names = ["Home", "My Resume", "VTR ChatGPT", "Need Help?"]
     page = st.radio("Navigation", page_names)
 
 if page == "Home":
@@ -49,54 +53,7 @@ if page =="Need Help?":
         msg.chat_message("assistant").write(f"Echo: {prompt}")
 
 
-if page =="BMI Calculator":
-    st.title('Welcome to BMI Calculator')
-    with col1:
-        weight = st.number_input("Enter your weight (in kgs)")
-        status = st.radio('Select your height format: ',
-                      ('cms', 'meters', 'feet'))
-        if(status == 'cms'):
-            height = st.number_input('Centimeters')
-            try:
-                bmi = weight / ((height/100)**2)
-            except:
-                st.text("Enter some value of height")
-     
-        elif(status == 'meters'):
-        
-            height = st.number_input('Meters')
-     
-            try:
-                bmi = weight / (height ** 2)
-            except:
-                st.text("Enter some value of height")
-     
-        else:
-       
-            height = st.number_input('Feet')
-     
-        
-            try:
-                bmi = weight / (((height/3.28))**2)
-            except:
-                st.text("Enter some value of height")
-     
-    
-        if(st.button('Calculate BMI')):
-            st.text("Your BMI Index is {}.".format(bmi))
-        with col2:
-            if(bmi < 16):
-                st.error("You are Extremely Underweight")
-            elif(bmi >= 16 and bmi < 18.5):
-                st.warning("You are Underweight")
-            elif(bmi >= 18.5 and bmi < 25):
-                st.success("Healthy")
-            elif(bmi >= 25 and bmi < 30):
-                st.warning("Overweight")
-            elif(bmi >= 30):
-                st.error("Extremely Overweight")
-    
-
+st.sidebar.page_link("pages/Youtube_Harvesting.py", label="Youtube_Harvesting")
 
 
 
